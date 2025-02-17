d5ec993049d5c29eb7769fc82e7ad69a7c9b84cd
pythonvista <nablintech@gmail.com>
2025-02-16 18:44:40 -0800

    updated

--- components/Home/About/PastProject.vue
+++ components/Home/About/PastProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="px-10 bg-accent2 border-t border-black">
+  <section class="xs:px-5 md:px-10 bg-accent2 border-t border-black">
     <div class="flex flex-col w-full items-center justify-center py-5">
       <div class="flex w-full items-center justify-center">
         <div class="border-b border-black w-[10%]"></div>
@@ -10,13 +10,13 @@
         <div class="border-b border-black w-[10%]"></div>
       </div>
       <span
-        class="text-5xl tracking-widest md:font-semibold xs:font-medium text-orange-400"
+        class="text-2xl md:text-5xl tracking-widest md:font-semibold xs:font-medium text-orange-400"
         >Past Projects</span
       >
     </div>
 
-    <div class="content py-10">
-        <span class=" text-lg tracking-wider text-center">
+    <div class="content py-4">
+        <p class=" text-lg  text-center">
             In July 2015, TAAF partnered with the FCT primary healthcare board to give safe motherhood information and mama kits to women in PHC kubwa. <br>
 
             Still with technical support from the FCT Primary Healthcare Board in partnership with the Kabash Love Foundation, in October 2015 we took advocacy on safe motherhood to Dakwa community in Bwari area council Abuja. We also gave mama kits to pregnant women to encourage them attend antenatal clinics. One major challenges women in low income communities face during pregnancy and childbirth is purchasing the essentials for delivery. This is one major reasons why women give birth at home instead in health centers. <br>
@@ -24,15 +24,15 @@
             Previous Projects
             In February 2016 still in partnership with the FCT Primary Health Care Board, TAAF took advocacy and support to women in Kabusa IDP camp. The FCT primary health care board gave us technical support to immunize children, gave information on water hygiene and sanitation especially in pregnancy and HIV testing and screening. Pregnant women were also given mama kits.
 
-        </span>
+        </p>
     </div>
 
 
     <div class="md:py-15 px-0 md:grid md:grid-cols-2 w-full h-full gap-5 text-center sm:text-start">
   <!-- Text Section (second on small screens) -->
   <div class="flex flex-col w-full justify-center items-center sm:items-start gap-4 p-5 order-1 sm:order-2">
-    <span class="font-semibold text-3xl">Previous Projects</span>
-    <span class="text-lg w-full">
+    <span class="font-semibold xs:text-2xl md:text-3xl">Previous Projects</span>
+    <span class="text-lg xs:text-center w-full">
       In February 2016, still in partnership with the FCT Primary Health Care Board, TAAF took advocacy and support to women in Kabusa IDP camp. The FCT primary health care board gave us technical support to immunize children, gave information on water hygiene and sanitation, especially in pregnancy and HIV testing and screening. Pregnant women were also given mama kits.
     </span>
   </div>
